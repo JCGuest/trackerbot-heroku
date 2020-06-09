36865540f2c38f408b44a5d20b2ee040b9309ddb
JCGuest <jcguest90@gmail.com>
2020-06-09 09:26:30 -0500

    initialize

---
+++ db/schema.rb
@@ -0,0 +1,15 @@
+
+
+ActiveRecord::Schema.define(version: 2020_06_09_135821) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
