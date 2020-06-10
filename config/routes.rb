268a75154a76b2d23ca22a080b45328129acbc88
JCGuest <jcguest90@gmail.com>
2020-06-10 12:38:12 -0500

    setting up for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users
-
-  get '/login', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create', as: "sessions"
-  get '/logout', to: 'sessions#destroy'
-  get '/auth/:provider/callback' => 'sessions#create'
-  get '/sessions', to: 'sessions#new'
-  get 'clerks', to: 'clerks#new'
-
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  
+  resources :users, only: [:create, :show, :index]
 end
