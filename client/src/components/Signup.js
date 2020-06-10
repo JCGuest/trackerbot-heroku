4e512afeb76d67a089a23ada0cd3107cd858fb95
JCGuest <jcguest90@gmail.com>
2020-06-10 15:05:36 -0500

    styles for home and forms

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -15,19 +15,19 @@ export default class Signup extends React.Component {
     render() {
         return (
         <main>
-	<form>
-	
-		<div className="field">
-			<input type="email" name="email" className="input" placeholder={this.state.email}/>
-		</div>
-
-		<div className="field">
-			<input type="password" name="password" className="input" placeholder="Password"/> 
-		</div>
-	</form>
-
-    <button>Sign Up</button>
-</main>	
+            <div className='logo'>
+                <h4>>_TRACKERBOT</h4>
+            </div>
+	        <form>
+                <div className="field">
+                    <input type="email" name="email" className="input" placeholder={this.state.email}/>
+                </div>
+                <div className="field">
+                    <input type="password" name="password" className="input" placeholder="Password"/> 
+                </div>
+            </form>
+            <button>Sign Up</button>
+        </main>	
 )
     }
 }
