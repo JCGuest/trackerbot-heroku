b45a107c873d9684a5a545d9a26c18084d7bf2b1
JCGuest <jcguest90@gmail.com>
2020-06-10 19:16:22 -0500

    convert back to non-api for sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: 'trackerbot_json_api_session'
+if Rails.env === 'production' 
+    Rails.application.config.session_store :cookie_store, key: '_trackerbot', domain: 'trackerbot-json-api'
+  else
+    Rails.application.config.session_store :cookie_store, key: '_trackerbot' 
+  end
