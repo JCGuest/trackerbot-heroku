3bd43b62ee25c80d2d2b859825e37d9d291703d9
JCGuest <jcguest90@gmail.com>
2020-06-13 11:28:22 -0500

    items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_232226) do
+ActiveRecord::Schema.define(version: 2020_06_11_153140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_06_10_232226) do
     t.string "username"
   end
 
+  add_foreign_key "items", "users"
 end
