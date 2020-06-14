fe58189b6395228455fc4128e7d86d97173d74db
JCGuest <jcguest90@gmail.com>
2020-06-13 20:54:56 -0500

    thunk for loding user

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import usersReducer from './usersReducer'
+import { Provider } from 'react-redux';
 
-const store = createStore(usersReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// const store = createStore(usersReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  usersReducer,
+  composeEnhancer(applyMiddleware(thunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/usersReducer.js
+++ client/src/usersReducer.js
@@ -1,15 +1,23 @@
-export default function usersReducer( state = { isLoggedIn: false, user: {}}, action) {
+export default function usersReducer( state = { isLoggedIn: false, user: {}, loading: false}, action) {
     // debugger
     switch (action.type) {
+        case "LOADING_STATUS":
+            return {
+                isLoggedIn: false,
+                user: {},
+                loading: true
+            }
         case "LOG_IN":
             return {
                 isLoggedIn: action.isLoggedIn,
-                user: action.user
+                user: action.user,
+                loading: false
             }
         case "LOG_OUT":
             return {
                 isLoggedIn: action.isLoggedIn,
-                user: action.user
+                user: action.user,
+                loading: false
             }
         default: return state;
     };
