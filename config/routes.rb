4367802c1984806a5c1d61d5a6ec07197fa875b5
JCGuest <jcguest90@gmail.com>
2020-06-15 17:43:11 -0500

    items controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   post '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
 
-  resources :users, only: [:create, :show, :index]
+  resources :users, only: [:create, :show, :index] do 
+    resources :items, only: [:create, :show, :index]
+  end
+
 end
