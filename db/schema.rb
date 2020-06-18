5fc3b9f47d256f573d6ec453119dd3858ad80a1e
JCGuest <jcguest90@gmail.com>
2020-06-17 19:59:53 -0500

    rmove email from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_153140) do
+ActiveRecord::Schema.define(version: 2020_06_17_081223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2020_06_11_153140) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
