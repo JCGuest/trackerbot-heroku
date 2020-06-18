cebdef89f4d35700440abece10af73e1e35ed594
JCGuest <jcguest90@gmail.com>
2020-06-18 00:33:07 -0500

    additem message and errors

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from './Logo';
 import Navbar from './Navbar';
+import axios from 'axios';
 
 class AddItem extends React.Component {
 
@@ -8,7 +9,9 @@ class AddItem extends React.Component {
         super(props);
         this.state = {
             name: '',
-            location: ''
+            location: '',
+            message: '',
+            errors: ''
         }
     };
 
@@ -20,15 +23,65 @@ class AddItem extends React.Component {
         console.log(this.state)
     };
 
-    handleSubmit = () => {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // const { name, location } = this.state
+        let item = {
+            name: this.state.name,
+            location: this.state.location
+        }
+        // let axiosConfig = {
+        //     headers: {
+        //         'Content-Type': 'application/json;charset=UTF-8',
+        //         "Access-Control-Allow-Origin": "*",
+        //     }
+        //   };
+        // debugger
+        axios.post(`http://localhost:3001/users/${this.props.user.id}/items`, {item}, {withCredentials:true})
+        .then(json => {
+            if (json.data.logged_in) {
+                this.setState({
+                    ...this.state,
+                    message: ` saved name=${json.data.item.name}  location=${json.data.item.location} to database`
+                })
+            } else {
+                this.setState({
+                    ...this.state,
+                    errors: json.data.errors
+                })
+            }
+        })
+        .catch(error => console.log('api errors:', error))
+    };
 
-    }
+    handleErrors = () => {
+        return (
+            <div className='error-div'>
+              {this.state.errors.map(error => {
+                  return (
+                  <text className='error' key={error}>ERROR: {error}{<br></br>}</text>
+                  ) 
+              })};
+          </div>
+        )
+    };
+
+    handleMessage = () => {
+        return (
+            <div className='error-div'>
+                <text className='error' >CONFIRMATION: {this.state.message}{<br></br>}</text>
+            </div>
+            )
+    };
 
     render() {
         return (
             <div className='addItem'>
                 <Logo/>
                 <Navbar/>
+                <div className='message'>
+                    {this.state.message? this.handleMessage() : null}
+                </div>
                 <form onSubmit={this.handleSubmit}>
                     <div className='field'>
                         <input onChange={this.handleChange}
@@ -42,6 +95,9 @@ class AddItem extends React.Component {
                     </div>
                     <button type="submit" className='text'>next ></button>
                 </form>
+                <div>
+                    {this.state.errors? this.handleErrors() : null}
+                </div>
             </div>
         ) 
     };  
