920a16f011bb19a9f61cfd8950b132e6522762b7
JCGuest <jcguest90@gmail.com>
2020-06-28 00:57:52 -0500

    remove compose and redux-dev-tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './usersReducer'
 import { Provider } from 'react-redux';
 
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  usersReducer,
-  composeEnhancer(applyMiddleware(thunk)),
-);
-
-// const store = createStore(usersReducer, applyMiddleware(thunk)) 
+const store = createStore(usersReducer, applyMiddleware(thunk)) 
 
 
 ReactDOM.render(
