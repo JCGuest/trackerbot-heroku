853e7aeeb832f874144bbd0c150f095cab24c759
JCGuest <jcguest90@gmail.com>
2020-06-28 01:45:13 -0500

    not passing props correctly, changed app.js props block

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
             <Route exact path="/login"          render={ props => (<Login    {...props} loginUser={this.props.loginUser} loggedInStatus={this.props.isLoggedIn} /> )} />
             <Route exact path="/signup"         render={ props => (<Signup   {...props} loginUser={this.props.loginUser} loggedInStatus={this.props.isLoggedIn}/> )} />
             <Route exact path="/menu"           render={ props => (<Menu     {...props} user={this.props.user} loggedInStatus={this.props.isLoggedIn} logoutUser={this.props.logoutUser}  /> )} />
-            <Route exact path="/tracker"        render={ props => (<Tracker  {...props} user={user} items={items} addItems={this.props.addItems} logoutUser={this.props.logoutUser} /> )} />
+            <Route exact path="/tracker"        render={ props => (<Tracker  {...props} user={this.props.user} items={this.props.items} addItems={this.props.addItems} logoutUser={this.props.logoutUser} /> )} />
             <Route exact path="/add_item"       render={ props => (<AddItem  {...props} user={user} /> )} />
           </Switch>
         </Router>
