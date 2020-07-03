a9bdf1dd8d98f327a6e024edc80711b606eba8fa
JCGuest <jcguest90@gmail.com>
2020-07-03 00:13:56 -0500

    attempting to deploy with redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './usersReducer'
 import { Provider } from 'react-redux';
 
 
-const store = createStore(usersReducer, applyMiddleware(thunk)) 
+// const store = createStore(usersReducer, applyMiddleware(thunk)) 
 
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//   usersReducer,
-//   composeEnhancer(applyMiddleware(thunk)),
-// );
+const store = createStore(
+  usersReducer,
+  composeEnhancer(applyMiddleware(thunk)),
+);
 
 
 
