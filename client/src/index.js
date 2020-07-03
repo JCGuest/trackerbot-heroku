6a96d5b0c60dd483ea7003a6ed2fd35e8fcd5d9d
JCGuest <jcguest90@gmail.com>
2020-07-03 00:05:38 -0500

    remove compose import

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './usersReducer'
 import { Provider } from 'react-redux';
