b4233526740d16860d9c51379e3f21b6bbe49f37
JCGuest <jcguest90@gmail.com>
2020-07-03 00:53:17 -0500

    remove console.log, explicit route worked!!

--- client/src/actions/fetchItems.js
+++ client/src/actions/fetchItems.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 export const fetchItems = (userId) => {
     let id = userId
     return (dispatch) => {
-        console.log(id)
     axios.get(`/users/${id}/items`, {withCredentials:true})
     .then(json => {
         if (json.data.logged_in) {
             dispatch({type: "ADD_ITEMS", isLoggedIn: true, items: json.data.items})
         } else {
-            console.log(json)
             dispatch({type: "LOG_OUT", isLoggedIn: false, user: {}})
         }
     })
